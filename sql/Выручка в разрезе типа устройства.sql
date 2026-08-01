6577673a560077b1d96da01c78f53cc6fcac545f
Иван Самонов <157703056+IvanSamonov@users.noreply.github.com>
2026-08-01 14:08:18 +0300

    Update Выручка в разрезе типа устройства.sql

--- sql/Выручка в разрезе типа устройства.sql
+++ sql/Выручка в разрезе типа устройства.sql
@@ -1,9 +1,9 @@
 -- Распределение выручки и количества заказов по типу устройства. Вывод:
--- тип устройства device_type_canonica;
--- общая выручка с заказов total_revenue;
--- количество заказов total_orders;
--- средняя стоимость заказа avg_revenue_per_order;
--- доля выручки для каждого устройства от общего значения revenue_share, округлённая до трёх знаков после точки.
+-- 1. тип устройства device_type_canonica;
+-- 2. общая выручка с заказов total_revenue;
+-- 3. количество заказов total_orders;
+-- 4. средняя стоимость заказа avg_revenue_per_order;
+-- 5. доля выручки для каждого устройства от общего значения revenue_share.
 SELECT
 	device_type_canonical,
 	sum(revenue) AS total_revenue,
@@ -12,5 +12,5 @@ SELECT
 	round((sum(revenue) / (SELECT sum(revenue) FROM  afisha.purchases WHERE currency_code = 'rub'))::NUMERIC, 3) AS revenue_share
 FROM afisha.purchases
 WHERE currency_code = 'rub'
-GROUP BY 1
-ORDER BY 5 DESC
+GROUP BY device_type_canonical
+ORDER BY revenue_share DESC
